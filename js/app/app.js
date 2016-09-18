f64f51a6450bc76a3dc1ef8885a23679172d798f
Yasser shaikh <yasser.s@media.net>
2016-09-18 13:24:19 +0530

    Added some colors.

--- js/app/app.js
+++ js/app/app.js
@@ -1,15 +1,15 @@
-var tabId, tabUrl;
-chrome.tabs.getSelected(null, function (tab) {
-    tabId = tab.id;
-    tabUrl = tab.url;
-});
+// var tabId, tabUrl;
+// chrome.tabs.getSelected(null, function (tab) {
+//     tabId = tab.id;
+//     tabUrl = tab.url;
+// });
 
 $(document)
     .ready(function () {
-        var cssModifier = new CssModifier(tabId, tabUrl);
-        cssModifier.update("body{background:pink !important;}");
+        // var cssModifier = new CssModifier(tabId, tabUrl);
+        // cssModifier.update("body{background:pink !important;}");
 
         var codeMirrorUtil = new CodeMirrorUtil();
-        codeMirrorUtil.load(".title");
+        codeMirrorUtil.load("editor");
     });
 
--- js/app/codemirror-util.js
+++ js/app/codemirror-util.js
@@ -1,11 +1,20 @@
+var rock = 
+{
+    beautify_in_progress: false,
+    editor: null // codemirror editor
+};
 function CodeMirrorUtil() {    
 }
 
 CodeMirrorUtil.prototype.load = function (identifier) {
-	var myCodeMirror = CodeMirror(document.getElementById("editor"), {
-		value: "#body{font-size:12px;}\n",
-		mode:  "css",
-		lineNumbers: true,
-		theme: "zenburn"
-	});
+	var textArea = $('#editor')[0];    
+    rock.editor = CodeMirror.fromTextArea(textArea, {
+            theme: 'ambiance',
+            lineNumbers: true,
+            mode:  "css",
+			matchBrackets: true,
+			lineNumbers: true,
+			height:300
+        });
+    rock.editor.focus();
 };
