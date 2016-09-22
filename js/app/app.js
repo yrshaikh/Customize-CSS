aff0f6b1a34852bac32ba267d1bf7fda274b68fe
Ali Rizvi <ali.ri@media.net>
2016-09-22 11:20:13 +0530

    some fixes

--- js/app/app.js
+++ js/app/app.js
@@ -1,15 +1,27 @@
-// var tabId, tabUrl;
+ var tabId, tabUrl;
 // chrome.tabs.getSelected(null, function (tab) {
 //     tabId = tab.id;
 //     tabUrl = tab.url;
 // });
+
+
 
 $(document)
     .ready(function () {
         // var cssModifier = new CssModifier(tabId, tabUrl);
         // cssModifier.update("body{background:pink !important;}");
 
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+            console.log(tab.url);
+        });
+
         var codeMirrorUtil = new CodeMirrorUtil();
         codeMirrorUtil.load("editor");
+
+        $("#apply").click(function(){
+            var cssModifier = new CssModifier(tabId, tabUrl);
+            cssModifier.update(rock.editor.getValue());
+        });
+
     });
 
